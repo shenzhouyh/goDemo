1fdc30872afa3b47340d71ce8c3b2c315c63ff7b
shenzhouyh <286817442@qq.com>
2021-08-01 10:55:18 +0800

    使用panic抛出异常，使用recover捕获异常

--- FirstDay/makeDemo.go
+++ FirstDay/makeDemo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 )
@@ -20,61 +21,69 @@ func main() {
 	copySlice()
 
 	deleteMap()
+
+	//调用异常
+	handleException()
 }
 
 //创建切片，相当于java中的数组
-func makeSlice(){
+func makeSlice() {
 	strings := make([]string, 3)
-	strings[0]="第一位"
-	strings[1]="第二位"
-	strings[2]="第三位"
+	strings[0] = "第一位"
+	strings[1] = "第二位"
+	strings[2] = "第三位"
 	fmt.Println(strings)
 	i := make([]string, 5)
-	i[1]="这是第一位";
-	fmt.Println(i,"这是新声明的切片")
+	i[1] = "这是第一位"
+	fmt.Println(i, "这是新声明的切片")
 }
+
 //创建map，相当于java中的map，具有key和value的键值对
-func makeMap()  {
+func makeMap() {
 	m := make(map[int]string, 5)
-	m[11]="11"
-	m[22]="22"
-	m[22]="33"
+	m[11] = "11"
+	m[22] = "22"
+	m[22] = "33"
 	fmt.Println(m)
 }
+
 //创建chan，管道，可以向里面写数据
 func makeChan() {
 	ints := make(chan int)
 	//关闭管道
 	close(ints)
 }
+
 //使用new的方式创建map
 func newMap() {
 	newMap := new(map[int]string)
 	makeMap := make(map[int]string)
 	//指针类型
-	fmt.Println("newMap",reflect.TypeOf(newMap))
+	fmt.Println("newMap", reflect.TypeOf(newMap))
 	//引用类型
-	fmt.Println("makeMap",reflect.TypeOf(makeMap))
+	fmt.Println("makeMap", reflect.TypeOf(makeMap))
 }
+
 //调用slice的append方法，对切片进行扩充
-func appendElementForSlice()  {
+func appendElementForSlice() {
 	strings := make([]string, 2)
-	strings[0]="id-1"
-	strings[1]="id-2"
-	fmt.Println("切片扩容之前的长度为：",len(strings))
+	strings[0] = "id-1"
+	strings[1] = "id-2"
+	fmt.Println("切片扩容之前的长度为：", len(strings))
 	strings = append(strings, "id-3")
 
 	fmt.Println(strings)
-	fmt.Println("切片扩容之后的长度为：",len(strings))
+	fmt.Println("切片扩容之后的长度为：", len(strings))
 }
+
 //复制切片内容
 func copySlice() {
-	sliceSource :=make([]string,3)
-	sliceSource[0]="0"
-	sliceSource[1]="1"
-	sliceSource[2]="2"
-	sliceTarget :=make([]string,3)
-	copy(sliceTarget,sliceSource)
+	sliceSource := make([]string, 3)
+	sliceSource[0] = "0"
+	sliceSource[1] = "1"
+	sliceSource[2] = "2"
+	sliceTarget := make([]string, 3)
+	copy(sliceTarget, sliceSource)
 	fmt.Println(sliceTarget)
 	fmt.Println(reflect.TypeOf(sliceTarget))
 	fmt.Println(reflect.TypeOf(sliceSource))
@@ -82,9 +91,37 @@ func copySlice() {
 
 func deleteMap() {
 	m := make(map[int]string)
-	m[1]="1"
+	m[1] = "1"
 	fmt.Println(m)
-	delete(m,1)
+	delete(m, 1)
 	fmt.Println(m)
 }
 
+/**
+异常处理：panic   recover
+*/
+
+func handleException() {
+	//捕获异常信息，保证程序的顺利执行
+	/*defer func() {
+		message := recover()
+		fmt.Println("异常信息为：",message)
+	}()*/
+	defer coverPanic()
+	//panic("I am panic")
+	panic(errors.New("this is an error"))
+}
+
+func coverPanic() {
+	message := recover()
+	switch message.(type) {
+	case string:
+		fmt.Println("string message", message)
+	case error:
+		fmt.Println("error message", message)
+	default:
+		fmt.Println("unknown message")
+
+	}
+
+}
