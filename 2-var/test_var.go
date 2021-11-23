36ed01acf955f1408ee8e3348437ab6c510f2454
yinhao <286817442@qq.com>
2021-11-23 09:47:51 +0800

    补充多变量赋值

--- 2-var/test_var.go
+++ 2-var/test_var.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	// 第一种：
 	var name string = "你好"
-	fmt.Printf("type of  name=#{name}\n")
+	fmt.Printf("value of name = %s type of name=%T\n", name, name)
 	fmt.Println("name的数据类型为：", reflect.TypeOf(name))
 	fmt.Println(name)
 
@@ -26,5 +26,17 @@ func main() {
 
 	//第四种：省去数据类型
 	var length = 10
-	fmt.Print("length的长度是：", length)
+	fmt.Println("length的长度是：", length)
+
+	//多变量声明
+
+	var a, b, c = 100, 200, 300
+	fmt.Println("a=", a, "b=", b, "c=", c)
+	var (
+		e = 100
+		f = 200
+		g = 300
+	)
+
+	fmt.Println("e=", e, "f=", f, "g=", g)
 }
